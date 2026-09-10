bedb9cbc4c06b27684355abf61ab4ca859bcdc30
javiereduardomoranjurado <javiereduardomoranjurado@gmail.com>
2026-09-09 22:17:39 -0500

    test: Pruebas unitarias del ImageAtom

---
+++ projects/design-system-bootstrap/src/lib/mocks/image.mocks.ts
@@ -0,0 +1,9 @@
+import { ImageType } from '../core/interfaces/core.interface';
+
+export const MOCK_IMAGE_TYPES: ImageType[] = ['fluid', 'thumbnail'];
+
+export const MOCK_IMAGE_SRC = 'https://placehold.co/200x200';
+export const MOCK_IMAGE_ALT = 'Imagen del producto';
+export const MOCK_IMAGE_ALT_UPDATE = 'Imagen del producto actualizada';
+export const MOCK_IMAGE_WIDTH = 100;
+export const MOCK_IMAGE_HEIGHT = 120;
